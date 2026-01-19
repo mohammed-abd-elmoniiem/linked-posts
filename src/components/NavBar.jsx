f256c656cd742bf4555b28e738b4d8f8c80362be
mohamed abd elmoniem <mohammed.abd.elmoniiem@gmail.com>
2026-01-19 19:09:37 +0200

    fix navbar bug

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -80,7 +80,7 @@ export default function NavBar(){
 
                 className="w-8 h-8 rounded-full  flex items-center justify-center"
                 onClick={()=>{
-                    setMode(!modez)
+                    setMode(!mode)
                 }}
                 
                 >
